21b5fe5c3cacff9c14a516f7ab8d183405b3fd2f
Ebrahimios <himoom80@gmail.com>
2025-07-09 16:18:05 +0400

    fix link

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import { APP_BASE_HREF } from '@angular/common';
 import { environment } from './environments/environment';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
@@ -15,7 +14,6 @@ bootstrapApplication(AppComponent, {
     provideAnimations(),
     provideRouter(routes),
     provideHttpClient(),
-    { provide: APP_BASE_HREF, useValue: '/AITU-Students/' }, // تحديد baseHref
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore())
